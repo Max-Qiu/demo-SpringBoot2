5d2534237538620bfb2de6be7ddc79fc259b9eaf
Max-Qiu <qzhing@126.com>
2021-05-26 23:50:40 +0800

    添加 SetOperations HashOperations 示例代码

---
+++ demo-SpringBoot-Redis/src/test/java/com/maxqiu/demo/HashOperationsTest.java
@@ -0,0 +1,135 @@
+package com.maxqiu.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisOperations;
+
+/**
+ * @author Max_Qiu
+ */
+@SpringBootTest
+public class HashOperationsTest {
+    @Autowired
+    private RedisOperations<String, Object> redisOperations;
+
+    @Resource(name = "stringRedisTemplate")
+    private HashOperations<String, String, String> stringHashOperations;
+
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, String, Integer> integerHashOperations;
+
+    /**
+     * HSET 给哈希散列添加字段和值
+     * 
+     * HSETNX 给哈希散列添加字段和值（仅字段不存在）
+     */
+    @Test
+    @Order(1)
+    void set() {
+        // HSET 给哈希散列添加字段和值
+        // 单个
+        stringHashOperations.put("user", "id", "1");
+        stringHashOperations.put("user", "name", "tom");
+        // 多个
+        stringHashOperations.putAll("user", Map.of("email", "tom@126.com", "age", "18"));
+
+        // HSETNX 给哈希散列添加字段和值（仅字段不存在）
+        Boolean flag1 = stringHashOperations.putIfAbsent("user", "address", "jiangsu");
+        assertTrue(flag1);
+        Boolean flag2 = stringHashOperations.putIfAbsent("user", "address", "jiangsu");
+        assertFalse(flag2);
+
+        // 删除数据
+        redisOperations.delete("user");
+    }
+
+    /**
+     * HGET 获取哈希散列的指定字段的值
+     * 
+     * HMGET 获取哈希散列的多个字段的值
+     * 
+     * HGETALL 获取哈希散列的所有字段与值
+     * 
+     * HKEYS 获取哈希散列的所有字段
+     * 
+     * HVALS 获取哈希散列的所有值
+     */
+    @Test
+    @Order(2)
+    void get() {
+        // 准备数据
+        stringHashOperations.putAll("user", Map.of("id", "1", "name", "tom", "email", "tom@126.com", "age", "18"));
+
+        // HGET 获取哈希散列的指定字段的值
+        String name = stringHashOperations.get("user", "name");
+        assertEquals("tom", name);
+
+        // HMGET 获取哈希散列的多个字段的值
+        List<String> list = stringHashOperations.multiGet("user", Arrays.asList("name", "age", "email", "address"));
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        // HGETALL 获取哈希散列的所有字段与值
+        Map<String, String> user1 = stringHashOperations.entries("user");
+        for (String s : user1.keySet()) {
+            System.out.println(s + "  " + user1.get(s));
+        }
+
+        // HKEYS 获取哈希散列的所有字段
+        Set<String> keys = stringHashOperations.keys("user");
+        for (String key : keys) {
+            System.out.println(key);
+        }
+
+        // HVALS 获取哈希散列的所有值
+        List<String> values = stringHashOperations.values("user");
+        for (String value : values) {
+            System.out.println(value);
+        }
+
+        // 删除数据
+        redisOperations.delete("user");
+    }
+
+    @Test
+    @Order(3)
+    void other() {
+        // 准备数据
+        stringHashOperations.putAll("user", Map.of("id", "1", "name", "tom", "email", "tom@126.com", "age", "18"));
+
+        // HEXISTS 哈希散列是否存在指定字段
+        Boolean hasName = stringHashOperations.hasKey("user", "name");
+        assertTrue(hasName);
+
+        // HDEL 删除哈希散列的指定字段
+        Long delete = stringHashOperations.delete("user", "id", "email");
+        assertEquals(2, delete);
+
+        // HLEN 哈希散列的长度
+        Long size = stringHashOperations.size("user");
+        assertEquals(2, size);
+
+        // HSTRLEN 哈希散列的字段的长度
+        Long length = stringHashOperations.lengthOfValue("user", "name");
+        assertEquals(3, length);
+
+        // HINCRBY 增减哈希散列中指定字段的值
+        Long increment = integerHashOperations.increment("user", "age", 1);
+        assertEquals(19, increment);
+    }
+}
---
+++ demo-SpringBoot-Redis/src/test/java/com/maxqiu/demo/SetOperationsTest.java
@@ -0,0 +1,177 @@
+package com.maxqiu.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SetOperations;
+
+import com.maxqiu.demo.entity.User;
+
+/**
+ * @author Max_Qiu
+ */
+@SpringBootTest
+public class SetOperationsTest {
+    @Autowired
+    private RedisOperations<String, Object> redisOperations;
+
+    /**
+     * 值是String类型的集合
+     *
+     * 自定义的RedisTemplate在格式化String时会多出引号
+     *
+     * 需要使用Spring内置的StringRedisTemplate
+     */
+    @Resource(name = "stringRedisTemplate")
+    private SetOperations<String, String> stringSetOperations;
+
+    /**
+     * 值是对象类型的集合
+     *
+     * 使用自定义的RedisTemplate，将值格式化成JSON
+     */
+    @Resource(name = "redisTemplate")
+    private SetOperations<String, User> userSetOperations;
+
+    /**
+     * 值是数字类型的集合
+     *
+     * 自定义的RedisTemplate可以格式化Integer
+     */
+    @Resource(name = "redisTemplate")
+    private SetOperations<String, Integer> integerSetOperations;
+
+    /**
+     * 测试基本操作
+     */
+    @Test
+    @Order(1)
+    void normal() {
+        // SADD 添加元素到集合中
+        Long add = stringSetOperations.add("set", "a", "b", "c", "d", "e");
+        assertEquals(5, add);
+
+        // SCARD 集合大小
+        Long size = stringSetOperations.size("set");
+        assertEquals(add, size);
+
+        // SMEMBERS 列出集合中的元素
+        Set<String> set = stringSetOperations.members("set");
+        assertNotNull(set);
+        assertEquals(5, set.size());
+
+        // SISMEMBER 存在指定的元素
+        Boolean member = stringSetOperations.isMember("set", "a");
+        assertTrue(member);
+
+        // SREM 删除指定元素
+        Long remove = stringSetOperations.remove("set", "a");
+        assertEquals(1, remove);
+
+        // SMOVE 移动指定元素
+        Boolean move = stringSetOperations.move("set", "b", "set2");
+        assertTrue(move);
+
+        // SRANDMEMBER 随机取出一个或多个元素
+        // 取出一个元素
+        String set1 = stringSetOperations.randomMember("set");
+        System.out.println(set1);
+        // 取出多个元素且不重复
+        Set<String> set3 = stringSetOperations.distinctRandomMembers("set", 2);
+        assertEquals(2, set3.size());
+        // 取出多个元素但可能有重复
+        List<String> list = stringSetOperations.randomMembers("set", 2);
+        assertEquals(2, list.size());
+
+        // SPOP 随机取出一个或多个元素并删除
+        String set2 = stringSetOperations.pop("set");
+        System.out.println(set2);
+        List<String> list2 = stringSetOperations.pop("set", 2);
+        assertNotNull(list2);
+        assertEquals(2, list2.size());
+
+        // 删除数据
+        assertFalse(redisOperations.hasKey("set"));
+        redisOperations.delete("set2");
+    }
+
+    /**
+     * 测试不同类型
+     */
+    @Test
+    @Order(2)
+    void type() {
+        // 对象类型
+        Long add = userSetOperations.add("users", new User(1, "tom", new BigDecimal(185)),
+            new User(2, "tom", new BigDecimal(183)));
+        Long size = userSetOperations.size("users");
+        assertEquals(add, size);
+
+        // 数字类型
+        Long nums = integerSetOperations.add("nums", 1, 2, 3, 3);
+        assertEquals(3, nums);
+
+        // 删除数据
+        redisOperations.delete(Arrays.asList("users", "nums"));
+    }
+
+    /**
+     * SINTER 交集
+     * 
+     * SUNION 并集
+     * 
+     * SDIFF 差集
+     */
+    @Test
+    @Order(3)
+    void aggregate() {
+        // 准备数据
+        stringSetOperations.add("set1", "a", "b", "c");
+        stringSetOperations.add("set2", "a", "d", "f");
+
+        // SINTER 交集
+        Set<String> intersect = stringSetOperations.intersect("set1", "set2");
+        // stringSetOperations.intersect(Arrays.asList("set1", "set2"));
+        // stringSetOperations.intersect("set1", Arrays.asList("set2"));
+        assertEquals(1, intersect.size());
+        assertTrue(intersect.contains("a"));
+
+        // SINTERSTORE 计算交集并存入目标集合
+        Long intersectAndStore = stringSetOperations.intersectAndStore("set1", "set2", "set3");
+        assertEquals(1, intersectAndStore);
+
+        // SUNION 并集
+        Set<String> union = stringSetOperations.union("set1", "set2");
+        assertEquals(5, union.size());
+
+        // SUNIONSTORE 计算并集并存入目标集合
+        Long unionAndStore = stringSetOperations.unionAndStore("set1", "set2", "set4");
+        assertEquals(5, unionAndStore);
+
+        // SDIFF 差集
+        Set<String> difference = stringSetOperations.difference("set1", "set2");
+        assertEquals(2, difference.size());
+        assertTrue(difference.contains("b"));
+
+        // SDIFFSTORE 计算差集并存入目标集合
+        Long differenceAndStore = stringSetOperations.differenceAndStore("set1", "set2", "set5");
+        assertEquals(2, differenceAndStore);
+
+        // 删除数据
+        redisOperations.delete(Arrays.asList("set1", "set2", "set3", "set4", "set5"));
+    }
+}
