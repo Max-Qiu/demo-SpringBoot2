096cc97d0c384abda652d0d24ad9510b660308ac
Max-Qiu <qzhing@126.com>
2021-07-07 22:03:13 +0800

    删除无用的模板文件，重命名自动填充配置文件

--- demo-SpringBoot-MybatisPlus/src/main/java/com/maxqiu/demo/handler/AutoFillMetaObjectHandler.java
+++ demo-SpringBoot-MybatisPlus/src/main/java/com/maxqiu/demo/handler/AutoFillMetaObjectHandler.java
@@ -9,26 +9,34 @@ import org.springframework.stereotype.Component;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 
 /**
+ * 自动填充
+ * 
  * @author Max_Qiu
  */
 @Component
-public class TimeMetaObjectHandler implements MetaObjectHandler {
-
+public class AutoFillMetaObjectHandler implements MetaObjectHandler {
     /**
      * 根据 MySQL datetime 字段设置的长度值设置精度
+     *
+     * 解决 Java 和 MySQL 时间精度不一样的问题（即使用MySQL的时间精度格式化一下）
+     *
+     * 数据库中的时间字段长度设置为几时，下方最后就有几个S。例如：datetime(4) -> uuuu-MM-dd'T'HH:mm:ss.SSSS
      */
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
 
+    /**
+     * 创建时自动插入
+     *
+     * @param metaObject
+     */
     @Override
     public void insertFill(MetaObject metaObject) {
         System.out.println("插入时自动填充");
 
         // 方案1 起始版本 3.3.0(推荐使用)
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-
-        // 解决 Java 和 MySQL 时间精度不一样的问题（即使用MySQL的时间精度格式化一下）
-        // LocalDateTime parse = LocalDateTime.parse(LocalDateTime.now().format(FORMATTER), FORMATTER);
-        // this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, parse);
+        // this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        LocalDateTime parse = LocalDateTime.parse(LocalDateTime.now().format(FORMATTER), FORMATTER);
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, parse);
 
         // 方案2 起始版本 3.3.3(推荐)
         // this.strictInsertFill(metaObject, "createTime", new Supplier<LocalDateTime>() {
@@ -42,12 +50,19 @@ public class TimeMetaObjectHandler implements MetaObjectHandler {
         // this.fillStrategy(metaObject, "createTime", LocalDateTime.now());
     }
 
+    /**
+     * 更新时自动插入
+     *
+     * @param metaObject
+     */
     @Override
     public void updateFill(MetaObject metaObject) {
         System.out.println("修改时自动填充");
 
         // 方案1 起始版本 3.3.0(推荐使用)
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        // this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        LocalDateTime parse = LocalDateTime.parse(LocalDateTime.now().format(FORMATTER), FORMATTER);
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, parse);
 
         // 方案2 起始版本 3.3.3(推荐)
         // this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
