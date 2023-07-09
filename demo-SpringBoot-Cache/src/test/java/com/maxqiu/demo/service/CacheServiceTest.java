d831a5fc3d9513cee04eae1d33b3dacfd7f95048
Max-Qiu <qzhing@126.com>
2023-07-09 22:30:04 +0800

    删除文档，同步代码

--- demo-SpringBoot-Cache/src/test/java/com/maxqiu/demo/service/CacheServiceTest.java
+++ demo-SpringBoot-Cache/src/test/java/com/maxqiu/demo/service/CacheServiceTest.java
@@ -1,6 +1,10 @@
 package com.maxqiu.demo.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
